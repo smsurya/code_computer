be36f1c9b45aaede191cb97a5ba0c337ad96dd17
smsurya <smsurya008@gmail.com>
2024-02-09 15:52:43 +0530

    Update Computer.java
    the computer is changes something

--- src/org/computer/Computer.java
+++ src/org/computer/Computer.java
@@ -3,7 +3,10 @@ package org.computer;
 public class Computer {
 	public void computerModel() {
 		System.out.println("ComputerModel is Windows 10 ");
-		
+	public static void main(String[] args) {
+		Desktop d = new Desktop();
+		d.desktopSize();
+		d.computerModel();	
 	}
 
 }
